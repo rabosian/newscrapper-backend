d09ec4806cc9c531af6f565c23b790816bd99b1a
Debora-k <debora0104k@gmail.com>
2024-11-13 16:07:13 -0700

    added getArticles in controller

--- src/routes/articleApi.js
+++ src/routes/articleApi.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { newsAPI_Everything } from '../services/newsApiService.js';
+import articleController from '../controllers/articleController.js';
 
 router.get('/', newsAPI_Everything);
 
