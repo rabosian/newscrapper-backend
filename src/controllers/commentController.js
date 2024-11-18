9e4419eee13c6af3b3883aa92b9b246ba94aa9ad
rabosian <msann1356@gmail.com>
2024-11-17 19:08:10 -0500

    update added, delete modified, PUT path added

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -22,11 +22,51 @@ export const createComment = async (req, res) => {
   }
 };
 
-// no longer in use - comments will be populated in getArticle
+export const updateComment = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+    const { contents, likeRequest } = req.body;
+
+    const findComment = await Comment.findById(id);
+    if (!findComment) throw new Error('Comment not found');
+
+    // likes 업데이트, likeRequest=true 이면 like 수정 기능 작동
+    if (likeRequest) {
+      const likedIndex = findComment.likes.findIndex((item) =>
+        item.userId.equals(userId)
+      );
+      if (likedIndex === -1) {
+        findComment.likes.push({ userId });
+      } else {
+        findComment.likes.splice(likedIndex, 1);
+      }
+    }
+
+    // contents 업데이트
+    if (contents) {
+      if (!findComment.userId.equals(userId))
+        throw new Error('Only comment creator can update');
+
+      findComment.contents = contents;
+    }
+
+    await findComment.save();
+    res.status(200).json({ status: 'success', findComment });
+  } catch (err) {
+    res.status(400).json({ status: 'failed', error: err.message });
+  }
+};
+
 export const getCommentsByArticle = async (req, res) => {
   try {
     const { articleId } = req.query;
-    const commentList = await Comment.find({ articleId }).populate('userId');
+    const commentList = await Comment.find({ articleId })
+      .populate({
+        path: 'userId',
+        select: 'name',
+      })
+      .sort({ createdAt: -1 });
     res.status(200).json({ status: 'success', commentList });
   } catch (err) {
     res.status(400).json({ status: 'failed', error: err.message });
@@ -35,8 +75,10 @@ export const getCommentsByArticle = async (req, res) => {
 
 export const deleteComment = async (req, res) => {
   try {
-    const { id } = req.body;
-    await Comment.findOneAndDelete(id);
+    const { id } = req.params;
+    const findComment = await Comment.findOneAndDelete(id);
+    if (!findComment) throw new Error('Comment not found');
+    res.status(200).json({ status: 'success' });
   } catch (err) {
     res.status(400).json({ status: 'Failed', error: err.message });
   }
--- src/routes/commentApi.js
+++ src/routes/commentApi.js
@@ -4,11 +4,13 @@ import {
   createComment,
   deleteComment,
   getCommentsByArticle,
+  updateComment,
 } from '../controllers/commentController.js';
 import { authenticate } from '../controllers/authController.js';
 
 router.post('/', authenticate, createComment);
 router.get('/', getCommentsByArticle);
+router.put('/:id', updateComment);
 router.delete('/:id', deleteComment);
 
 export default router;
